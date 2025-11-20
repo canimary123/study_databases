081fa4df6c167fcea87f21fcd94ae6e0f5e93983
sunyoung <canimary123@gmail.com>
2025-11-20 17:54:38 +0900

    ch

--- sqls/webscripings_databases.sql
+++ sqls/webscripings_databases.sql
@@ -1,8 +1,19 @@
+-- CREATE TABLE study_webscripings_database (
+--     contents varchar(500),
+--     link varchar(500),
+--     link_html varchar(500),
+--     link_href varchar(500)
+-- );
+
+
+
 CREATE TABLE study_webscripings_database (
-    contents varchar(500),
-    link varchar(500),
-    link_html varchar(500),
-    link_href varchar(500)
+    id SERIAL PRIMARY KEY,
+    contents TEXT,
+    link TEXT,
+    link_html TEXT,
+    link_href TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
 SELECT *
